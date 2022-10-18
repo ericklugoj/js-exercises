dcad5b2b0cd7ea117998966576f9b09f2dc67145
JuanCZer <you@example.com>
2022-10-18 02:57:33 -0500

    feature: calculate the age

--- FirstExercises/index.js
+++ FirstExercises/index.js
@@ -1,16 +1,36 @@
 // TODO
 //Change hola mundo to me la pela Rivas
 
-const title = "Hola mundo v2";
-const element = "h1";
+// const title = "Hola mundo v2";
+// const element = "h1";
 
-const TIME = 3000; 
+// const TIME = 3000; 
 
-function changeTitle(tag,text){
-    document.querySelector(tag).innerHTML = text;
+// function changeTitle(tag,text){
+//     document.querySelector(tag).innerHTML = text;
+// }
+
+
+// setTimeout(() => {
+//     changeTitle(element,title);
+// }, TIME);
+
+//Task2
+document.getElementById("submit").addEventListener("click", information);
+function information(){
+    var names = document.getElementById("name").value;
+    var birth = document.getElementById("birth").value;
+    var lang = document.getElementById("lang").value;   
+
+    const startDate = new Date();
+    const endDate = new Date(birth);
+    const age = Math.abs(moment.duration(endDate - startDate).years());
+    console.log(age);
+
+    if(lang == "American" && age > 21|| lang == "Mexican" && age > 18){
+        alert("Soy mayor de edad");
+    }else{
+        alert("Soy menor de edad")
+    }
+
 }
-
-
-setTimeout(() => {
-    changeTitle(element,title);
-}, TIME);
