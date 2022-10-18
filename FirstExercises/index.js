64a14b7f4d2621d1cbabc9eeb9baa113dec1126b
JuanCZer <you@example.com>
2022-10-18 00:45:07 -0500

    feature: resolve first exercies

---
+++ FirstExercises/index.js
@@ -0,0 +1,4 @@
+// TODO
+//Change hola mundo to me la pela Rivas
+const title =  document.querySelector("h1").innerHTML = "me la pela Rivas";
+const title3 =  document.querySelector(".hola").innerHTML = "Te amo Lugo";
